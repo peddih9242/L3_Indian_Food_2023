128bc0ffbca43fed37f32806a7965a4890a3b0b2
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2023-09-21 14:42:09 +1200

    separated banner

---
+++ banner_navigation.php
@@ -0,0 +1,37 @@
+<div class="box banner">
+            
+    
+<a href="index.php"><h1>Indian Food Database</h1></a>
+</div>    <!-- / banner -->
+
+<!-- Navigation goes here.  Edit BOTH the file name and the link name -->
+<div class="box nav">
+
+<div class="linkwrapper">
+    <div class="commonsearches">
+        <a href="index.php?page=all_results&search=all">All Foods</a> | 
+        <a href="index.php?page=all_results&search=recent">Recent</a> | 
+        <a href="index.php?page=all_results&search=random">Random</a> 
+    </div>  <!-- / common searches -->
+
+    <div class="topsearch">
+        
+        <!-- Quick Search -->           
+        <form method="post" action="quick_search.php" enctype="multipart/form-data">
+
+            <input class="search quicksearch" type="text" name="quick_search" size="40" value="" required placeholder="Quick Search..." />
+
+            <input class="submit" type="submit" name="find_quick" value="&#xf002;" />
+
+        </form>     <!-- / quick search -->
+        
+    </div>  <!-- / top search -->
+    
+    <div class="topadmin">
+        <a href="#">Log In</a>
+        
+    </div>  <!-- / top admin -->
+    
+</div>  <!--- / link wrapper -->
+
+</div>    <!-- / nav -->        
--- index.php
+++ index.php
@@ -25,7 +25,7 @@
     <meta name="author" content="Put your name here">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>DB Name</title>
+    <title>Indian Food Database</title>
     
     <!-- Edit the link below / replace with your chosen google font -->
     <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
@@ -40,45 +40,7 @@
     
     <div class="wrapper">
     
-
-        
-        <div class="box banner">
-            
-    
-            <h1>DB Name</h1>
-        </div>    <!-- / banner -->
-
-        <!-- Navigation goes here.  Edit BOTH the file name and the link name -->
-        <div class="box nav">
-            
-            <div class="linkwrapper">
-                <div class="commonsearches">
-                    <a href="#l">Item</a> | 
-                    <a href="#">Item</a> | 
-                    <a href="#">Item</a> 
-                </div>  <!-- / common searches -->
-            
-                <div class="topsearch">
-                    
-                    <!-- Quick Search -->           
-                    <form method="post" action="quick_search.php" enctype="multipart/form-data">
-
-                        <input class="search quicksearch" type="text" name="quick_search" size="40" value="" required placeholder="Quick Search..." />
-
-                        <input class="submit" type="submit" name="find_quick" value="&#xf002;" />
-
-                    </form>     <!-- / quick search -->
-                    
-                </div>  <!-- / top search -->
-                
-                <div class="topadmin">
-                    <a href="#">Log In</a>
-                    
-                </div>  <!-- / top admin -->
-                
-            </div>  <!--- / link wrapper -->
-            
-        </div>    <!-- / nav -->        
+    <?php include("banner_navigation.php") ?>
         
         <div class="box main">
             
