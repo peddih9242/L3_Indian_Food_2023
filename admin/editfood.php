f34788f5627e9d6484353371c0eea8226cd22556
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2023-10-30 12:29:01 +1300

    Update editfood.php

--- admin/editfood.php
+++ admin/editfood.php
@@ -3,7 +3,7 @@
 if (isset($_SESSION['admin'])) {
 
 // retrieve subjects and authors to populate combo box
-include("sub_author.php");
+// include("sub_author.php");
 
 // retrieve current values for quote
 $ID = $_REQUEST['ID'];
@@ -29,51 +29,105 @@ $state_ID = $edit_results_rs['State_ID'];
         <p><textarea name="food" placeholder="Food (required)" required><?php echo $food ?></textarea></p>
         
         <div class="important">
-            If you edit an author, it will change the author name for the quote being edited. It does not edit the author name on all quotes attributed to that author.
+            If you edit a food, it will change the author name for the quote being edited. It does not edit the author name on all quotes attributed to that author.
         </div>
 
         <br />
-
-        <div class="autocomplete">
-            <input name="author_full" id="author_full" value="<?php echo str_replace('  ', ' ', $author_full_name) ?>" /></p>
-        </div>
         
-        <div class="light_blue">
-            Blank subjects appear as n/a. You can either edit these / add a subject or leave them as n/a.
-        </div>
+        <select name="diet" id="diet" placeholder="Diet (required)" required >
 
-        <br/>
+        <option value="" disabled selected>Diet</option>
 
-        <div class="autocomplete">
-            <input name="subject1" id="subject1" value="<?php echo $subject_1 ?>" required /></p>
-        </div>
+        <!-- get options from database -->
 
-        <div class="autocomplete">
-            <input name="subject2" id="subject2" value="<?php echo $subject_2 ?>" />
-        </div>
+        <?php 
+
+        $diet_sql = "SELECT * FROM `diet`
+        WHERE `Diet_ID` ORDER BY `Diet` ASC
+        ";
+        $diet_query = mysqli_query($dbconnect, $diet_sql);
+        $diet_rs = mysqli_fetch_assoc($diet_query);
+
+        do {
+            ?>
+            <option value="<?php echo $diet_rs['Diet_ID']; ?>"><?php echo $diet_rs['Diet']; ?></option>
+
+        <?php
+        }
+
+        while($diet_rs=mysqli_fetch_assoc($diet_query))
+
+        ?>
+
+        </select>
+
+        <select name="course" id="course" placeholder="Course (required)" required >
+
+        <option value="" disabled selected>Course</option>
+
+        <?php 
+
+        $course_sql = "SELECT * FROM `course`
+        WHERE `Course_ID` ORDER BY `Course` ASC
+        ";
+        $course_query = mysqli_query($dbconnect, $course_sql);
+        $course_rs = mysqli_fetch_assoc($course_query);
+
+        do {
+            ?>
+            <option value="<?php echo $course_rs['Course_ID']; ?>"><?php echo $course_rs['Course']; ?></option>
+
+        <?php
+        }
+
+        while($course_rs=mysqli_fetch_assoc($course_query))
+
+        ?>
+
+        </select>
+
+        <select name="flavor" id="flavor" placeholder="Flavor" required>
+
+        <option value="" disabled selected>Flavor</option>
+
+        <?php 
+
+        $flavor_sql = "SELECT * FROM `flavor`
+        WHERE `Flavor_ID` NOT LIKE '1' ORDER BY `Flavor` ASC
+        ";
+        $flavor_query = mysqli_query($dbconnect, $flavor_sql);
+        $flavor_rs = mysqli_fetch_assoc($flavor_query);
+
+        do {
+            ?>
+            <option value="<?php echo $flavor_rs['Flavor_ID']; ?>"><?php echo $flavor_rs['Flavor']; ?></option>
+
+        <?php
+        }
+
+        while($flavor_rs=mysqli_fetch_assoc($flavor_query))
+
+        ?>
+
+        </select>
 
         <br /><br />
 
         <div class="autocomplete">
-            <input name="subject3" id="subject3" value="<?php echo $subject_3 ?>" />
+            <input name="state" id="state" value="<?php echo $state_ID ?>" />
         </div>
 
         <br /><br />
 
-        <input class="form-submit" type="submit" name="submit" value="Edit Quote" />
+        <input class="form-submit" type="submit" name="submit" value="Edit Food" />
     </form>
 
     <script>
         <?php include ("autocomplete.php") ?>
 
         // Arrays containing lists
-        var all_tags = <?php print("$all_subjects")?>;
-        autocomplete(document.getElementById("subject1"), all_tags);
-        autocomplete(document.getElementById("subject2"), all_tags);
-        autocomplete(document.getElementById("subject3"), all_tags);
-        
-        var all_author = <?php print("$all_authors") ?>;
-        autocomplete(document.getElementById("author_full"), all_author);
+        var all_state = <?php print("$all_states") ?>;
+        autocomplete(document.getElementById("state"), all_state)
 
     </script>
 
